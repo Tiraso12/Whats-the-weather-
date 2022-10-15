65f3440418ead9ce117a03de0161476f72984f69
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-10-15 13:46:14 -0400

    adding jquery to make easier the DOM implementation with future forecast

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -2,6 +2,7 @@ var btnEl = document.getElementById('btn');
 var apiKey = "60c731acdd16090e93a89f08aee6e2e4";
 var cityName;
 var cityEl = document.querySelector('#city');
+var futureForecast =[];
 
 
 
@@ -19,6 +20,7 @@ function getCity() {
                         var latitude = latitude.toString();
                         var longitude = longitude.toString();
                         getApi(latitude, longitude);
+                        futureApi(latitude, longitude);
                     });
             } else {
                 alert("Location Not Found");
@@ -28,21 +30,38 @@ function getCity() {
 
 function getApi(latitude, longitude) {
     var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude={part}&appid=" + apiKey;
-    
+
     fetch(requestUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        var imgCode = "http://openweathermap.org/img/wn/"+data.current.weather[0].icon+".png";
-            document.querySelector('#cityName').innerHTML ="City: " +" "+ cityName;
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var imgCode = "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + ".png";
+            document.querySelector('#cityName').innerHTML = "City: " + " " + cityName;
             document.querySelector("#img").setAttribute("src", imgCode);
-            document.querySelector('#tem').innerHTML ="Temperature: " +" "+ data.current.temp + "°F";
-            document.querySelector('#hum').innerHTML ="Humidity: " +" "+ data.current.humidity+ "%";
-            document.querySelector('#wind').innerHTML ="Wind: " +" "+ data.current.wind_speed + " MPH";
-            document.querySelector('#uv').innerHTML ="UV index: " +" "+ data.current.uvi;
+            document.querySelector('#tem').innerHTML = "Temperature: " + " " + data.current.temp + "°F";
+            document.querySelector('#hum').innerHTML = "Humidity: " + " " + data.current.humidity + "%";
+            document.querySelector('#wind').innerHTML = "Wind: " + " " + data.current.wind_speed + " MPH";
+            document.querySelector('#uv').innerHTML = "UV index: " + " " + data.current.uvi;
 
         })
 };
+
+function futureApi(latitude, longitude) {
+    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=" + apiKey;
+    fetch(requestUrl).then(function (response) {
+        response.json().then(function (data) {
+            futureForecast = data.list;
+
+            createFuture(futureForecast);
+        })
+    });
+}
+
+function createFuture(futureForecast) {
+    var col = $("<div></div>");
+
+    $("#future").append(col)
+}
 
 btnEl.addEventListener("click", getCity);
