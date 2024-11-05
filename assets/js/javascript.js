d65cd6118d9cedd33006d641bd03b6773c9b5c2e
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2024-11-05 13:31:48 -0500

    Refactor CSS styles and form layout

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -6,94 +6,132 @@ var futureForecast = [];
 
 
 
-function getCity() {
-    // var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityEl.value + "&limit=1&appid=" + apiKey;
+async function getCity() {
+    // let apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityEl.value + "&limit=1&appid=" + apiKey;
+    let apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + "chicago" + "&limit=1&appid=" + apiKey;
 
-    fetch(apiUrl)
-        .then((response) => {
-            if (response.ok) {
-                response.json()
-                    .then(function (data) {
-                        var latitude = data[0].lat;
-                        var longitude = data[0].lon;
-                        cityName = data[0].name;
-                        var latitude = latitude.toString();
-                        var longitude = longitude.toString();
-                        getApi(latitude, longitude);
-                        futureApi(latitude, longitude);
-                    });
-            } else {
-                alert("Location Not Found");
+    try {
+        const response = await fetch(apiUrl);
+        if(!response.ok){
+            throw new Error('Location Not Found');
+        }
+        const data =await response.json();
+        let cityLat = data[0].lat;
+        let cityLon = data[0].lon;
+        console.log(cityLat, cityLon);
+         cityLat = cityLat.toString();
+         cityLon = cityLon.toString();
+
+        
+        getWeather(cityLat, cityLon);
+        
+    } catch (error) {
+        console.error(error);
+    }
+
+    async function getWeather(lat, lon) {
+        const requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&appid=" + apiKey;
+
+        try {
+            const response = await fetch(requestUrl);
+            if(!response.ok){
+                throw new Error('Weather Not Found');
             }
-        })
-};
-
-function getApi(latitude, longitude) {
-    var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude={part}&appid=" + apiKey;
-
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            var imgCode = "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + ".png";
-            document.querySelector('#cityName').innerHTML = "City: " + " " + cityName;
-            document.querySelector("#img").setAttribute("src", imgCode);
-            document.querySelector('#tem').innerHTML = "Temperature: " + " " + data.current.temp + "°F";
-            document.querySelector('#hum').innerHTML = "Humidity: " + " " + data.current.humidity + "%";
-            document.querySelector('#wind').innerHTML = "Wind: " + " " + data.current.wind_speed + " MPH";
-
-        })
-};
-
-function futureApi(latitude, longitude) {
-    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=" + apiKey;
-    fetch(requestUrl).then(function (response) {
-        response.json().then(function (data) {
-            futureForecast = data.list;
-
-            createFuture(futureForecast);
-        })
-    });
-}
-
-function createFuture(futureForecast) {
-    console.log(futureForecast);
-    for (let i = 0; i < 5; i++) {
-
-
-        var col = $("<div></div>");
-
-        var cityTitle = $('<h4></h4>');
-        cityTitle.attr("id", "cityName");
-        cityTitle.text("DATE");
-
-        var image = $('<img></img>');
-        var imgCode = "http://openweathermap.org/img/wn/" + futureForecast[i].weather[0].icon + ".png";
-        image.attr({ src: imgCode, id: "img" });
-
-        var temp = $('<p></p>');
-        temp.attr('id', "temp").text('Temperature: ' + futureForecast[i].main.temp + '');
-
-
-        var hum = $('<p></p>');
-        hum.attr('id', '').text('Humidity:'+futureForecast[i].main.humidity+'%');
-
-        var wind = $('<p></p>');
-        wind.attr('id', '').text('Wind:' + futureForecast[i].wind.speed + 'MPH',);
-
-
-
-
-
-        col.append(cityTitle)
-        col.append(image);
-        col.append(temp);
-        col.append(hum);
-        col.append(wind);
-        $("#future").append(col)
+            const data = await response.json();
+            console.log(data);
+            
+        } catch (error) {
+            console.error(error);
+        }
 
     }
+    // fetch(apiUrl)
+    //     .then((response) => {
+    //         if (response.ok) {
+    //             response.json()
+    //                 .then(function (data) {
+    //                     var latitude = data[0].lat;
+    //                     var longitude = data[0].lon;
+    //                     cityName = data[0].name;
+    //                     var latitude = latitude.toString();
+    //                     var longitude = longitude.toString();
+    //                     getApi(latitude, longitude);
+    //                     futureApi(latitude, longitude);
+    //                 });
+    //         } else {
+    //             alert("Location Not Found");
+    //         }
+    //     })
+// }
 }
 
-btnEl.addEventListener("click", getCity);
+// function getApi(latitude, longitude) {
+//     var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude={part}&appid=" + apiKey;
+
+//     fetch(requestUrl)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             var imgCode = "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + ".png";
+//             document.querySelector('#cityName').innerHTML = "City: " + " " + cityName;
+//             document.querySelector("#img").setAttribute("src", imgCode);
+//             document.querySelector('#tem').innerHTML = "Temperature: " + " " + data.current.temp + "°F";
+//             document.querySelector('#hum').innerHTML = "Humidity: " + " " + data.current.humidity + "%";
+//             document.querySelector('#wind').innerHTML = "Wind: " + " " + data.current.wind_speed + " MPH";
+
+//         })
+// };
+
+// function futureApi(latitude, longitude) {
+//     var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=" + apiKey;
+//     fetch(requestUrl).then(function (response) {
+//         response.json().then(function (data) {
+//             futureForecast = data.list;
+
+//             createFuture(futureForecast);
+//         })
+//     });
+// }
+
+// function createFuture(futureForecast) {
+//     console.log(futureForecast);
+//     for (let i = 0; i < 5; i++) {
+
+
+//         var col = $("<div></div>");
+
+//         var cityTitle = $('<h4></h4>');
+//         cityTitle.attr("id", "cityName");
+//         cityTitle.text("DATE");
+
+//         var image = $('<img></img>');
+//         var imgCode = "http://openweathermap.org/img/wn/" + futureForecast[i].weather[0].icon + ".png";
+//         image.attr({ src: imgCode, id: "img" });
+
+//         var temp = $('<p></p>');
+//         temp.attr('id', "temp").text('Temperature: ' + futureForecast[i].main.temp + '');
+
+
+//         var hum = $('<p></p>');
+//         hum.attr('id', '').text('Humidity:'+futureForecast[i].main.humidity+'%');
+
+//         var wind = $('<p></p>');
+//         wind.attr('id', '').text('Wind:' + futureForecast[i].wind.speed + 'MPH',);
+
+
+
+
+
+//         col.append(cityTitle)
+//         col.append(image);
+//         col.append(temp);
+//         col.append(hum);
+//         col.append(wind);
+//         $("#future").append(col)
+
+//     }
+// }
+
+// btnEl.addEventListener("click", getCity);
+// getCity();
