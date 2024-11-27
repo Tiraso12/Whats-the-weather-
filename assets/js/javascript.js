c59b57f9b7cd326d9aac6cd1f3ba3f35ee3f8009
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2024-11-27 13:07:22 -0500

    Refactor weather display and improve API integration

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,13 +1,20 @@
-var btnEl = document.getElementById('btn');
-var apiKey = "60c731acdd16090e93a89f08aee6e2e4";
-var cityName;
-var cityEl = document.querySelector('#city');
-var futureForecast = [];
+let buttonElement = document.getElementById('btn');
+let apiKey = "60c731acdd16090e93a89f08aee6e2e4";
+let cityEl = document.querySelector('#city');
+let futureForecasts = [];
+
+let cityName = document.getElementById('cityName');
+let cityTemperature = document.getElementById('weather_temperature');
+let cityIcon = document.getElementById('weather_icon');
+
+
+
 
 
 
 async function getCity() {
-    let apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityEl.value + "&limit=1&appid=" + apiKey;
+    // let apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityEl.value + "&limit=1&appid=" + apiKey;
+    let apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + 'orlando' + "&limit=1&appid=" + apiKey;
 
     try {
         const response = await fetch(apiUrl);
@@ -17,23 +24,19 @@ async function getCity() {
         const data = await response.json();
         let cityLat = data[0].lat;
         let cityLon = data[0].lon;
-         cityLat = cityLat.toString();
-         cityLon = cityLon.toString();
 
         
-        getWeather(cityLat, cityLon);
-        
-    } catch (error) {
-        console.error(error);
+            getWeather(cityLat, cityLon);
+            
+        } catch (error) {
+            console.error(error);
+        }
     }
 
-    async function getWeather(lat, lon) { 
-        const requestUrl = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+ "&lon="+lon+"&appid=" + apiKey;
 
-        console.log(requestUrl);
-        
-
-
+ async function getWeather(lat, lon) { 
+        const requestUrl = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+ "&lon="+lon+"&appid=" + apiKey + "&units=imperial";
+    
         try {
             const response = await fetch(requestUrl);
             if(!response.ok){
@@ -42,42 +45,40 @@ async function getCity() {
             }
             const data = await response.json();
             console.log(data);
-            return data;
+            return displayWeather(data);
             
         } catch (error) {
             console.error(error);
         }
     }
 
+function displayWeather(data) {
+    cityName.textContent = data.name;    
+    cityTemperature.textContent = data.main.temp + "°F";
+    cityIcon.src = "http://openweathermap.org/img/wn/" + data.weather[0].icon +"@2x.png";
 
-    fetch(apiUrl)
-        .then((response) => {
-            if (response.ok) {
-                response.json()
-                    .then(function (data) {
-                        var latitude = data[0].lat;
-                        var longitude = data[0].lon;
-                        cityName = data[0].name;
-                        var latitude = latitude.toString();
-                        var longitude = longitude.toString();
-                        futureApi(latitude, longitude);
-                    });
-            } else {
-                alert("Location Not Found");
-            }
-        })
+
+
 }
 
-function futureApi(latitude, longitude) {
+
+
+function fetchFutureForecast(latitude, longitude) {
     var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=" + apiKey;
-    fetch(requestUrl).then(function (response) {
-        response.json().then(function (data) {
-            futureForecast = data.list;
-
-            createFuture(futureForecast);
+    fetch(requestUrl)
+        .then(function (response) {
+            if (!response.ok) {
+                throw new Error('Forecast Not Found');
+            }
+            return response.json();
         })
-    });
+        .then(function (data) {
+            futureForecasts = data.list;
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
 }
 
-btnEl.addEventListener("click", getCity);
+buttonElement.addEventListener("click", getCity);
 // getCity();
